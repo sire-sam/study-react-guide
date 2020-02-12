116307327f10476fbf1884d90b551fbd45b891e9
sire-sam <sire-sam@users.noreply.github.com>
2020-02-12 22:58:45 +0100

    Use React.Component.state to update clock time

--- src/top-bar/clock/Clock.tsx
+++ src/top-bar/clock/Clock.tsx
@@ -2,10 +2,18 @@ import React, { ReactNode } from 'react';
 import { ClockPropsInterface, ClockStatesInterface } from './clock.interfaces';
 
 class Clock extends React.Component<ClockPropsInterface, ClockStatesInterface> {
+  private tempo = 1000;
+  private interval?: number;
 
   constructor(props: ClockPropsInterface) {
     super(props);
     this.state = { time: new Date() };
+  }
+
+  private setCurrentTIme() {
+    this.setState({
+      time: new Date(),
+    })
   }
 
   render(): ReactNode {
@@ -15,6 +23,18 @@ class Clock extends React.Component<ClockPropsInterface, ClockStatesInterface> {
     </span>
     )
   }
+
+  componentDidMount() {
+    this.interval = window.setInterval(
+      () => this.setCurrentTIme(),
+      this.tempo,
+    );
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+
 }
 
 export default Clock;
