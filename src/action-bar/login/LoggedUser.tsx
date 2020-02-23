b698345cddc883c02e9dd0affa35d8b6f0c683b3
sire-sam <sire-sam@users.noreply.github.com>
2020-02-23 13:51:55 +0100

    Use hooks in LoggedUser component

--- src/action-bar/login/LoggedUser.tsx
+++ src/action-bar/login/LoggedUser.tsx
@@ -1,47 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Avatar from '../../user/Avatar';
 import { UserInterface } from '../../user/user.interface';
 
-export class LoggedUser<P extends { onRequestLogout: () => void, user: UserInterface }, S extends { isOpen: boolean }> extends Component<P, S> {
+interface LoggedUserPropsInterface {
+  onRequestLogout: () => void,
+  user: UserInterface
+}
 
-  constructor(props: P) {
-    super(props);
-    this.state = {
-      isOpen: false,
-    } as Readonly<S>;
-    this.logout = this.logout.bind(this);
-    this.toggle = this.toggle.bind(this);
+export function LoggedUser(props: LoggedUserPropsInterface) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function logout() {
+    props.onRequestLogout();
   }
 
-  logout() {
-    this.props.onRequestLogout();
+  function toggle() {
+    setIsOpen(!isOpen);
   }
 
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  }
+  return (
+    <div className="popover-area">
+      <button className="btn-hit-area" type="button" onClick={toggle}>
+        <Avatar
+          className="action-bar__icon"
+          user={props.user}/>
+      </button>
+      {
+        isOpen &&
+        <div className="emphasis popover">
+          <button
+            className={'btn'}
+            onClick={logout}>
+            Logout
+          </button>
+        </div>
+      }
+    </div>
+  );
 
-  render() {
-    return (
-      <div className="popover-area">
-        <button className="btn-hit-area" type="button" onClick={this.toggle}>
-          <Avatar
-            className="action-bar__icon"
-            user={this.props.user}/>
-        </button>
-        {
-          this.state.isOpen &&
-          <div className="emphasis popover">
-            <button
-              className={'btn'}
-              onClick={this.logout}>
-              Logout
-            </button>
-          </div>
-        }
-      </div>
-    );
-  }
 }
